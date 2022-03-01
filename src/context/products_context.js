23b95d7f1134a3f01871a12a1ec6666b93945da1
Slawek <hello@wellmade.online>
2022-03-01 21:19:38 +0100

    Moving on

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -21,6 +21,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -45,10 +48,20 @@ export const ProductsProvider = ({ children }) => {
     }
   };
 
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios(url);
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR, payload: error });
+    }
+  };
+
   useEffect(() => {
     fetchProducts(url);
   }, []);
-  return <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>{children}</ProductsContext.Provider>;
+  return <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}>{children}</ProductsContext.Provider>;
 };
 // make sure use
 export const useProductsContext = () => {
