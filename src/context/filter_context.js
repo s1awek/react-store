326a80448b89788b086fa079b89ec3fd96fc56b6
Slawek <hello@wellmade.online>
2022-03-02 12:58:11 +0100

    Moving on

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,29 +1,24 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
-import {
-  LOAD_PRODUCTS,
-  SET_GRIDVIEW,
-  SET_LISTVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+/** @format */
 
-const initialState = {}
+import React, { useEffect, useContext, useReducer } from 'react';
+import reducer from '../reducers/filter_reducer';
+import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW, UPDATE_SORT, SORT_PRODUCTS, UPDATE_FILTERS, FILTER_PRODUCTS, CLEAR_FILTERS } from '../actions';
+import { useProductsContext } from './products_context';
 
-const FilterContext = React.createContext()
+const initialState = { filtered_products: [], all_products: [] };
+
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
-  return (
-    <FilterContext.Provider value='filter context'>
-      {children}
-    </FilterContext.Provider>
-  )
-}
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+  return <FilterContext.Provider value={{}}>{children}</FilterContext.Provider>;
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};
